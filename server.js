389de48bfe339ef7c469c9e748402e7ee38458d5
SZtyro <SZtyro@users.noreply.github.com>
2020-06-29 11:05:55 +0200

    heroku3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/AngularJDBC'));
-app.options('*', cors())
+
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/AngularJDBC' }
   );
